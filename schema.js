37aeb308e3cbed9763d29241988b016101fa489c
balaji <balajier2006@gmail.com>
2025-02-19 11:47:44 +0530

    done

---
+++ schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const userschema = new mongoose.Schema({
+    name:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    roles:{type:String,default:[]}
+})
+
+const user = mongoose.model("user",userschema)
+
+const profileschema = new mongoose.Schema({
+    firstname:{type:String},
+    lastname:{type:String},
+    age:{type:Number}
+})
+const profile = mongoose.model("user",profileschema)
+
+const activitytracking = new mongoose.Schema({
+    lastlogin:{type:Date}
+})
+
+const activity = mongoose.model("user",activitytracking)
